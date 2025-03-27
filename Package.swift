79a4a32ae76e3c58a6b08104737b3c69fcc3c67e
Dutartre Thomas <33754435+ThomasDutartre@users.noreply.github.com>
2025-03-27 18:49:46 +0100

    Add github unit tests

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-﻿// swift-tools-version: 6.0
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import CompilerPluginSupport
